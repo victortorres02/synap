e0c090b01825e45cbd13ef30622c9ad7530c6c3f
DavidPaez <david.paezb201@tectijuana.edu.mx>
2023-10-13 14:34:26 -0700

    Ahora es Synap en lugar de W3

--- frontend/pages/footer.js
+++ frontend/pages/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="w3-padding-32 w3-black w3-center w3-margin-top">
             <h5>Si quieres contactarnos directamente aquí.</h5>
-            <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" rel="noopener noreferrer" className="w3-hover-text-green">w3.css</a></p>
+            <p>Powered by <a href="#" target="_blank" rel="noopener noreferrer" className="w3-hover-text-green">synap.com</a></p>
         </footer>
     )
 }
