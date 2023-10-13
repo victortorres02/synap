4c3f2fa06892ca4ec630be098ee0274dbb4b2d83
DavidPaez <david.paezb201@tectijuana.edu.mx>
2023-10-13 04:54:34 -0700

    Se añadió el archivo de Proyectos

---
+++ frontend/pages/proyectos.js
@@ -0,0 +1,18 @@
+import { NavBar } from "./NavBar";
+
+function Proyectos(){
+    return(
+        <div>
+            AADFAD
+        </div>
+    )
+}
+
+export default function main() {
+    return (
+        <div class="w3-black">
+            <NavBar />
+            <Proyectos />
+        </div>
+    )
+}
