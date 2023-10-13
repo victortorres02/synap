3d4d0a6885f5886e1845236275676bed382f3da4
Victor Torres <torres@ekiwa.org>
2023-10-13 07:56:01 -0700

    Retoques de lógica

--- synap-c/src/main.c
+++ synap-c/src/main.c
@@ -135,7 +135,13 @@ extern uint64_t accept_ver_role(SolParameters *params) {
     if (project_details->state != StateOpen)
 	    return ERROR_INVALID_ARGUMENT;
 
+    if (signature->data_len < sizeof(uint8_t))
+	    return ERROR_INVALID_ARGUMENT;
+
     uint8_t index = *signature->data;
+
+    if (index >= project_details->num_validators)
+	    return ERROR_INVALID_ARGUMENT;
 
     if (!SolPubkey_same(signature->owner, &project_details->validators[index])){
         return ERROR_INVALID_ARGUMENT;
@@ -145,7 +151,7 @@ extern uint64_t accept_ver_role(SolParameters *params) {
 
     if (!check_all_verifiers_accepted(state->data, project_details->num_validators))
     {
-	    SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
+	    SolAccountMeta arguments[] = {{ source_info->owner, true, true }};
 
 	    SolSignerSeeds signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
 
@@ -160,13 +166,15 @@ extern uint64_t accept_ver_role(SolParameters *params) {
     else
     {
 	    // Change to bidding state
-	    SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
+	    project_details->state = StateBidding;
+
+	    SolAccountMeta arguments[] = {{ source_info->owner, true, true }};
 
 	    SolSignerSeeds signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
 
 	    SolInstruction instruction = (SolInstruction) {source_info->key, arguments,
 		    SOL_ARRAY_SIZE(arguments), state->data,
-		    state->data_len};
+		    0};
 
 	    // temporary validator state
 	    return sol_invoke_signed(&instruction, params->ka, params->ka_num,
@@ -174,7 +182,7 @@ extern uint64_t accept_ver_role(SolParameters *params) {
     }
 }
 
-extern uint64_t begin_bid(SolParameters *params){
+extern uint64_t submit_bid(SolParameters *params){
     if(params->ka_num != 4){
         return ERROR_NOT_ENOUGH_ACCOUNT_KEYS;
     }
@@ -192,21 +200,27 @@ extern uint64_t begin_bid(SolParameters *params){
 
     ProjectDetails *project_details = (ProjectDetails*)pj->data;
 
-    if (project_details->state != StateOpen)
+    if (project_details->state != StateBidding)
         return ERROR_INVALID_ARGUMENT;
 
     BidState *bid_state = (BidState*)state->data;
 
-    if (bid_state->num_bidders > 255){
+    if (bid_state->num_bidders == 255){
         return ERROR_INVALID_ARGUMENT;
     }
 
+    if (info->data_len < sizeof(BidInfo))
+	    return ERROR_INVALID_ARGUMENT;
+
     BidInfo *bid_info = (BidInfo*)info->data;
 
-    void* new_state = sol_calloc((++bid_state->num_bidders) * sizeof(BidInfo), 1);
-    memcpy(bid_state, new_state, (bid_state->num_bidders - 1) * sizeof(BidInfo));
+    if (!SolPubkey_same(info->owner, &bid_info->bidder))
+	    return ERROR_INVALID_ARGUMENT;
 
-    memcpy(bid_info, new_state+(bid_state->num_bidders - 1) * sizeof(BidInfo), sizeof(BidInfo));
+    void* new_state = sol_calloc((++bid_state->num_bidders) * sizeof(BidInfo), 1);
+    sol_memcpy(bid_state, new_state, (bid_state->num_bidders - 1) * sizeof(BidInfo));
+
+    sol_memcpy(bid_info, new_state+(bid_state->num_bidders - 1) * sizeof(BidInfo), sizeof(BidInfo));
 
     SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
 
@@ -232,6 +246,9 @@ extern uint64_t select_bid(SolParameters *params){
     SolAccountInfo *selected_bidder = &params->ka[3];
 
     if (!SolPubkey_same(source_info->owner, pj->owner))
+	    return ERROR_INVALID_ARGUMENT;
+
+    if (!SolPubkey_same(source_info->owner, state->owner))
 	    return ERROR_INVALID_ARGUMENT;
 
     ProjectDetails *project_details = (ProjectDetails*)pj->data;
@@ -274,7 +291,7 @@ extern uint64_t begin_conflict(SolParameters *params){
 
     ProjectDetails *project_details = (ProjectDetails*)pj->data;
 
-    if (project_details->state != StateBidding) {
+    if (project_details->state != StateClosed) {
         return ERROR_INVALID_ARGUMENT;
     }
 
@@ -312,11 +329,44 @@ extern uint64_t create_proposal(SolParameters *params){
 
     ProjectDetails *project_details = (ProjectDetails*)pj->data;
 
+    if (!SolPubkey_same(source_info->owner, state->owner))
+	    return ERROR_INVALID_ARGUMENT;
+
     if (project_details->state != StateClosed) {
         return ERROR_INVALID_ARGUMENT;
     }
 
-    uint8_t  *sigs = sol_calloc((2+project_details->num_validators+7)/8, 1);
+
+    bool is_owner_or_dev = !SolPubkey_same(signature->owner, &project_details->owner)
+	    && !SolPubkey_same(signature->owner, &project_details->developer);
+
+    if (!is_owner_or_dev)
+    {
+	    bool found=false;
+	    for (size_t i=0; i < project_details->num_validators; ++i)
+		    if (SolPubkey_same(&project_details->validators[i], signature->owner))
+		    {
+			    found = true;
+			    break;
+		    }
+
+	    if (!found)
+		    return ERROR_INVALID_ARGUMENT;
+    }
+
+    if (state->data_len < sizeof(FinishProposal))
+	    return ERROR_INVALID_ARGUMENT;
+
+    FinishProposal *proposal=state->data;
+
+    if (proposal->owner_pay + proposal->developer_pay + proposal->validator_pay != project_details->amount)
+	    return ERROR_INVALID_ARGUMENT;
+
+    size_t new_size = state->data_len + sizeof(FinishProposal) + 1 + (project_details->num_validators+7)/8;
+    uint8_t *sigs = sol_calloc(new_size, 1);
+
+    sol_memcpy(state->data, sigs, state->data_len);
+    sol_memcpy(proposal, sigs + state->data_len, sizeof(FinishProposal));
 
     SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
 
@@ -324,7 +374,7 @@ extern uint64_t create_proposal(SolParameters *params){
 
     SolInstruction instruction = (SolInstruction) {source_info->key, arguments,
         SOL_ARRAY_SIZE(arguments), sigs,
-        (2+project_details->num_validators+7)/8};
+        new_size};
 
     // temporary validator state
     return sol_invoke_signed(&instruction, params->ka, params->ka_num,
@@ -346,47 +396,87 @@ extern uint64_t sign_proposal(SolParameters *params){
 
     ProjectDetails *project_details = (ProjectDetails*)pj->data;
 
-    if (project_details->state != StateBidding) {
+    if (!SolPubkey_same(source_info->owner, state->owner))
+	    return ERROR_INVALID_ARGUMENT;
+
+    if (project_details->state != StateClosed) {
         return ERROR_INVALID_ARGUMENT;
     }
 
+    if (signature->data_len < sizeof(uint8_t))
+	    return ERROR_INVALID_ARGUMENT;
+
     uint8_t index = *(uint8_t*)signature->data;
 
-    if (!SolPubkey_same(signature->owner, &project_details->validators[index])) {
+    bool is_owner_or_dev = !SolPubkey_same(signature->owner, &project_details->owner)
+	    && !SolPubkey_same(signature->owner, &project_details->developer);
+
+    if (!is_owner_or_dev && !project_details->conflict)
 	    return ERROR_INVALID_ARGUMENT;
-    }
 
-    state->data[index/8] |= 1 << (index%8);
-
-    if (!check_all_verifiers_accepted(state->data, project_details->num_validators))
+    uint8_t *p = state->data;
+    uint8_t *pp;
+    size_t i=index+1;
+    while (i != 0)
     {
-	    SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
+	    --i;
+	    pp = p;
+	    p += sizeof(FinishProposal);
+	    p += *p &1 ? 1 : 1 + (project_details->num_validators+7)/8;
 
-	    SolSignerSeeds signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
-
-	    SolInstruction instruction = (SolInstruction) {source_info->key, arguments,
-		    SOL_ARRAY_SIZE(arguments), state->data,
-		    state->data_len};
-
-	    // temporary validator state
-	    return sol_invoke_signed(&instruction, params->ka, params->ka_num,
-					   &signers_seeds, 1);
+	    if (p > state->data + state->data_len)
+		    return ERROR_INVALID_ARGUMENT;
     }
-    else
+
+    if (!is_owner_or_dev && *pp&1)
+	    return ERROR_INVALID_ARGUMENT;
+
+    if (is_owner_or_dev)
     {
-	    // Change to bidding state
-	    SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
+	    *pp |= SolPubkey_same(signature->owner, &project_details->owner)?2:4;
 
-	    SolSignerSeeds signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
-
-	    SolInstruction instruction = (SolInstruction) {source_info->key, arguments,
-		    SOL_ARRAY_SIZE(arguments), state->data,
-		    state->data_len};
-
-	    // temporary validator state
-	    return sol_invoke_signed(&instruction, params->ka, params->ka_num,
-					   &signers_seeds, 1);
+	    if (*pp == 7)
+		    goto do_proposal;
     }
+    else 
+    {
+	    for (i=0; i < project_details->num_validators; ++i)
+		    if (SolPubkey_same(&project_details->validators[i], signature->key))
+		    {
+			    pp[i/8] |= 1 << (i%8);
+		    }
+
+	    if (check_all_verifiers_accepted(pp, project_details->num_validators))
+		    goto do_proposal;
+    }
+
+    return ERROR_INVALID_ARGUMENT;
+
+do_proposal:
+    project_details->state = StateClosed;
+
+    SolAccountMeta arguments[] = {{ params->ka[0].owner, true, true }};
+
+    SolSignerSeeds signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
+
+    SolInstruction instruction = (SolInstruction) {source_info->key, arguments,
+	    SOL_ARRAY_SIZE(arguments), project_details,
+	    sizeof(ProjectDetails) + sizeof(SolPubkey)*project_details->num_validators};
+
+    // temporary validator state
+    if (sol_invoke_signed(&instruction, params->ka, params->ka_num,
+				   &signers_seeds, 1) != SUCCESS)
+	    return ERROR_INVALID_ARGUMENT;
+
+    signers_seeds = (SolSignerSeeds){valbits_seed, SOL_ARRAY_SIZE(valbits_seed)};
+
+    instruction = (SolInstruction) {source_info->key, arguments,
+	    SOL_ARRAY_SIZE(arguments), state->data,
+	    state->data_len};
+
+    // temporary validator state
+    return sol_invoke_signed(&instruction, params->ka, params->ka_num,
+				   &signers_seeds, 1);
 }
 
 extern uint64_t entrypoint(const uint8_t *input) {
@@ -409,7 +499,7 @@ extern uint64_t entrypoint(const uint8_t *input) {
 	  case ActAcceptVerificatorRole:
 		  return accept_ver_role(&params);
 	  case ActBeginBid:
-		  return begin_bid(&params);
+		  return submit_bid(&params);
 	  case ActSelectBid:
 		  return select_bid(&params);
 	  case ActCreateProposal:
