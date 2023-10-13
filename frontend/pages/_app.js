32900e673f1eb1f24c83e0c035b67db87dee7c33
DavidPaez <david.paezb201@tectijuana.edu.mx>
2023-10-12 20:44:56 -0700

    Se añadió el Chakra UI al Frontend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,22 @@
 import "@/styles/globals.css";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './demo.css';
+
+import * as React from 'react'
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 const activeChainId = ChainId.SolanaDevnet;
 
 export default function App({ Component, pageProps }) {
     return (
         <>
-            <ThirdwebProvider desiredChainId={activeChainId}>
-                <Component {...pageProps} />
-            </ThirdwebProvider>
+            <ChakraProvider>
+                <ThirdwebProvider desiredChainId={activeChainId}>
+                    <Component {...pageProps} />
+                </ThirdwebProvider>
+            </ChakraProvider>
         </>
     );
 }
